0abccabbe87d45ed6eb77b3924350597351ab536
MeteorologicalStation <aleksander.semenov@edu.rtu.lv>
2024-11-26 17:12:50 +0200

    Лежать+Сосать

--- Software/Scripts/Software.py
+++ Software/Scripts/Software.py
@@ -1,1 +1,19 @@
-print("Hello World!")
+import tkinter as tk
+
+# Create the main window
+root = tk.Tk()
+
+# Set the window title
+root.title("White Screen")
+
+# Set the window to full screen
+root.attributes('-fullscreen', True)
+
+# Set the background color to white
+root.configure(bg='white')
+
+# Add a key binding to close the application (e.g., pressing "Escape")
+root.bind("<Escape>", lambda event: root.destroy())
+
+# Run the application
+root.mainloop()
